d0559462e36b1596b60fa7aa261966d6ad20cc05
Francis Silva <79276987+VathanSilva@users.noreply.github.com>
2024-06-01 19:36:44 +0530

    Update sketch_may30a.ino

--- sketch_may30a.ino
+++ sketch_may30a.ino
@@ -2,10 +2,10 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "Redmi Note 12";           // Replace with your WiFi SSID
-const char* password = "Jack2000";   // Replace with your WiFi password
+const char* ssid = "";           // Replace with your WiFi SSID
+const char* password = "";   // Replace with your WiFi password
 
-const char* firestoreUrl = "https://firestore.googleapis.com/v1/projects/iot-smart-home-47e28/databases/(default)/documents/users/N7MO66yJf0dkOyBA3IJ0Tx5Bpfp2/homes/r7qWq8h1rezzrVJ31450/rooms/ROpRH9BfAhsb7uzyBgjW/devices/lwbNvP7hQvxUHGRl8fLH?key=AIzaSyA8qtxjY1OFtwN_B6dJSt7gg9GHnMG80ao";
+const char* firestoreUrl = "https://firestore.googleapis.com/v1/projects/{projectID}/databases/(default)/documents/users/{userID}/homes/{homeID}/rooms/{roomID}/devices/{deviceID}?key={API Key}";
 
 const int ledPin = 4; // GPIO pin for the LED
 String lastState = ""; // To store the last state
